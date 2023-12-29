d110d7df3d2b771c0a0e3b9b21f89ffd3d134d3e
ahrizona <missahrizona@outlook.com>
2023-12-29 08:08:17 -0600

    power through digital pin

--- drums/drum-stroke-capture.c
+++ drums/drum-stroke-capture.c
@@ -1,20 +1,56 @@
 #define ANALOG_IN_ESP32 34
+#define PIEZO_POWER_ESP32 32
+
 #define ANALOG_IN_RP2040 15
+#define PIEZO_POWER_RP2040 16
+
+#define ESP32 1
+#define RP2040 2
+
+#define POWER 1
+#define PIN 2
 
 bool note = false;
 unsigned long initialms = 0;
 unsigned long currms = 0;
 int currMax = 0;
+int board = RP2040;
+
+int curr_power_pin()
+{
+  if (board == ESP32)
+  {
+    return ANALOG_IN_ESP32;
+  }
+  else
+  {
+    return ANALOG_IN_RP2040;
+  }
+}
+
+int curr_input_pin()
+{
+  if (board == ESP32)
+  {
+    return PIEZO_POWER_ESP32;
+  }
+  else
+  {
+    return PIEZO_POWER_RP2040;
+  }
+}
 
 void setup()
 {
   Serial.begin(9600);
+  pinMode(curr_power_pin(), OUTPUT);
+  digitalWrite(curr_power_pin(), HIGH);
 }
 
 void loop()
 {
   // read the input on analog pin:
-  int sensorValue = analogRead(ANALOG_IN_ESP32);
+  int sensorValue = analogRead(curr_input_pin());
   // print out the value you read:
   if (sensorValue >= 100)
   {
